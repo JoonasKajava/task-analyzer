202b86841a6a869a2f0f2dc5cb52f33418beb111
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2025-08-31 23:56:46 +0300

    feat: implement very simple obsidian parser and add tests

--- src/backend/parsing/obsidian/obsidian.rs
+++ src/backend/parsing/obsidian/obsidian.rs
@@ -1,20 +1,87 @@
-use chrono::{DateTime, Local, Utc};
+use chrono::{NaiveDateTime, NaiveTime};
 
-
-
-struct ObsidianParser;
+use markdown::mdast::{ListItem, Node, Paragraph, Text};
+use regex::Regex;
+use thiserror::Error;
+pub struct ObsidianParser;
 
 // Temp
 #[derive(Debug, Default)]
-struct ActivityEntry {
+pub struct ActivityEntry {
     pub key: String,
     pub sub_key: Option<String>,
-    pub start_time: DateTime<Local>,
-    pub end_time: DateTime<Local>
+    pub start_time: NaiveTime,
+    pub end_time: NaiveTime,
 }
 
+#[derive(Debug, Error)]
+pub enum ObsidianParseError {
+    #[error("Failed to parse markdown")]
+    MarkdownParseError,
+    #[error("Failed to parse using regex")]
+    RegexParseError,
+}
+//
 impl ObsidianParser {
-    fn parse(&self, input: impl AsRef<str>) -> Result<Vec<ActivityEntry>, ()> {
-        Ok(vec![ActivityEntry::default()])
+
+    fn collect_text(node: &Node) -> String {
+        match node {
+            Node::Text(Text { value, .. }) => value.clone(),
+            Node::Paragraph(Paragraph { children, .. }) | Node::ListItem(ListItem {children, ..}) => children
+                .iter()
+                .map(ObsidianParser::collect_text)
+                .collect::<Vec<String>>()
+                .join(""),
+            _ => String::new(),
+        }
+    }
+
+    fn parse_task(listitem: &Node) -> Option<ActivityEntry> {
+        let text = ObsidianParser::collect_text(listitem);
+        let reg = Regex::new(r"(\d{2}:\d{2}) - (\d{2}:\d{2}).+?(JIRA:.+?)\s").expect("TODO");
+
+        let captures = reg.captures(&text)?;
+
+        let start = captures.get(1)?;
+        let end = captures.get(2)?;
+        let key = captures.get(3)?;
+
+        let start = NaiveTime::parse_from_str(start.as_str(), "%R").ok()?;
+        let end = NaiveTime::parse_from_str(end.as_str(), "%R").ok()?;
+
+        Some(ActivityEntry {
+            key:  key.as_str().to_string(),
+            sub_key: None,
+            start_time: start,
+            end_time: end,
+        })
+    }
+
+    fn find_tasks(node: &Node, tasks: &mut Vec<ActivityEntry>) {
+        match node {
+            Node::ListItem(_) => {
+                if let Some(activity) = ObsidianParser::parse_task(node) {
+                    tasks.push(activity);
+                }
+            },
+            _ => {
+                if let Some(children) = node.children() {
+                    children
+                        .iter()
+                        .for_each(|child| ObsidianParser::find_tasks(child, tasks))
+                }
+            }
+        }
+    }
+
+    pub fn parse(input: impl AsRef<str>) -> Result<Vec<ActivityEntry>, ObsidianParseError> {
+        let mdast = markdown::to_mdast(input.as_ref(), &markdown::ParseOptions::default())
+            .map_err(|_| ObsidianParseError::MarkdownParseError)?;
+
+        let mut tasks = vec![];
+
+        ObsidianParser::find_tasks(&mdast, &mut tasks);
+
+        Ok(tasks)
     }
 }
--- src/backend/parsing/obsidian/obsidian_parsing_tests.rs
+++ src/backend/parsing/obsidian/obsidian_parsing_tests.rs
@@ -2,9 +2,17 @@
 
 #[cfg(test)]
 mod tests {
+    use anyhow::Result;
+
+    use crate::backend::parsing::obsidian::obsidian::ObsidianParser;
 
 
-    fn test_simple() {
+    #[test]
+    fn test_simple() -> Result<()> {
+        let simple = include_str!("./obsidian_parsing_test_files/simple.md");
 
+        println!("{:?}", ObsidianParser::parse(simple));
+
+        Ok(())
     }
 }
